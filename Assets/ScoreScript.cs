a168580f2ed0daf9102b9847cfda4c21b31bce7f
tamasdeep <tamasdeep1624@gmail.com>
2019-04-17 17:36:03 -0400

    curser restart

--- Assets/ScoreScript.cs
+++ Assets/ScoreScript.cs
@@ -9,7 +9,7 @@ public class ScoreScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
